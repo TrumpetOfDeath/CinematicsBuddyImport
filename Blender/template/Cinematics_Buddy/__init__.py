d1cc3ec68dbc0cac8d0467c419957c5918d6fed3
TrumpetOfDeath <trumpetsteam@gmail.com>
2020-07-08 21:01:04 -0500

    Add application template.

---
+++ Blender/template/Cinematics_Buddy/__init__.py
@@ -0,0 +1,14 @@
+import bpy
+from bpy.app.handlers import persistent
+
+@persistent
+def load_handler(dummy):
+    pass
+
+
+def register():
+    bpy.app.handlers.load_factory_startup_post.append(load_handler)
+
+
+def unregister():
+    bpy.app.handlers.load_factory_startup_post.remove(load_handler)
